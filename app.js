0b83c548ba67a519f6d7e753a7b6f115117c36d6
Talha Shaikh <m-talha-shaikh@protonmail.com>
2023-02-20 08:01:04 +0500

    add mix functionality

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 let instrunments = document.getElementById('container').children;
-let lastSelect = null;
+let lastSound = null;
+let mixState = false;
+let mix = false;
+
+	console.log(mix)
+	console.log(mixState)
 
 let playSound = (event, box)=>{
+	if(mix === false){
+		if(lastSound != null){
+			lastSound.pause()
+		}
+	}
+
 	let id = event.target.getAttribute('id')
 	let idName = id + "Sound";
 	let sound = document.getElementById(idName);
 	sound.play();
-	lastSelect = event.target;
+	lastSound = sound;
 }
 
 for(let i = 0; i < 10 ; i++){
 	instrunments[i].addEventListener('click', playSound);
 }
+
+let mixer = document.getElementById('mix');
+mixer.addEventListener('click', ()=>{
+	if(mixState === true){
+		mixState = false;
+		mix = false;
+		mixer.style.backgroundColor = 'lavender';
+	}
+	else{
+		mixState = true;
+		mix = true;
+		mixer.style.backgroundColor = 'lightblue';
+	}
+	console.log(mix)
+	console.log(mixState)
+})
 
